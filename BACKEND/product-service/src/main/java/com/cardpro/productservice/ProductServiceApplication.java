81a42583111d3d8f7cd7da53ac00d7604dccae89
murarivamsharaj <chmurari73@gmail.com>
2026-08-11 19:57:30 +0530

    chore: stable checkpoint before routing fix

--- BACKEND/product-service/src/main/java/com/cardpro/productservice/ProductServiceApplication.java
+++ BACKEND/product-service/src/main/java/com/cardpro/productservice/ProductServiceApplication.java
@@ -3,9 +3,11 @@ package com.cardpro.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
